fc332629a25c65d22bd676d334a658c076941427
nohemi02 <zuritagonzaleznohemi@gmail.com>
2023-08-13 01:49:44 -0400

    the three pages partially finished with auxiliary information. Modified menu.

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,19 @@ const  emailToSend = "";
 const existDriver = "";
 const versionResponse ="";
 
+let boton = document.getElementById('flecha');
 
+boton.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+      document.querySelector('.btn-flotante').classList.add('mostrar');
+    } else {
+      document.querySelector('.btn-flotante').classList.remove('mostrar');
+    }
+  });
